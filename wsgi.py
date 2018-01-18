dd8813f361a28ec87915f67a3215a56d9f72881a
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2018-01-18 16:45:49 +0100

    Added probe

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import time
 
+start_time = None
 application = Flask(__name__)
-start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
 @application.route("/")
 def hello():
@@ -12,11 +12,12 @@ def hello():
 def test():
     return "Testing!"
 
-@application.route("/testprobe")
+@application.route("/probe")
 def test():
     return "I'm alive\nSince: {}".format(start_time)
 
 
 
 if __name__ == "__main__":
+    start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     application.run()
